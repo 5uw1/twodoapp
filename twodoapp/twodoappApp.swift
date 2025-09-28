fde10436b94b87aba639b103f6133b067e95e6b5
Suwijak Thanawiboon <suwijakza@gmail.com>
2025-09-28 13:19:42 +0200

    Initial Commit

---
+++ twodoapp/twodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  twodoappApp.swift
+//  twodoapp
+//
+//  Created by Suwijak Thanawiboon on 28/9/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct twodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
